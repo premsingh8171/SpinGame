b0ec2f0f98a2e66a34b3b52c9303d7ea2fb919c8
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2020-12-04 12:53:11 +0530

    first commit

---
+++ SpinAndWin/src/main/java/com/premsinghdaksha/spinandwin/model/LuckyItem.java
@@ -0,0 +1,13 @@
+package com.premsinghdaksha.spinandwin.model;
+
+/**
+ * Created by Er.prem singh on 12/8/2020.
+ */
+
+public class LuckyItem {
+    public String topText;
+    public String secondaryText;
+    public int secondaryTextOrientation;
+    public int icon;
+    public int color;
+}
